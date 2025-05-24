873f26862d0c34f61aca38cc555d30d43c8ac216
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2025-05-24 19:24:43 +0530

    Update Navigation.jsx

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -7,22 +7,12 @@ import { menuItems } from "../../utils/menuItems";
 function Navigation() {
   const navigate = useNavigate();
 
-  // Get user info from localStorage
-  const user = JSON.parse(localStorage.getItem("user"));
-  const username = user?.name || "User"; // fallback if name is not available
-
-  const handleSignout = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    navigate("/");
-  };
-
   return (
     <NavStyled>
       <div className="user-con">
         <img src={userAvatar} alt="User avatar" />
         <div className="text">
-          <h2>{username}</h2>
+          <h2>Bhargavi</h2>
           <br />
           <br />
         </div>
@@ -44,7 +34,7 @@ function Navigation() {
       </ul>
       <div className="bottom-nav">
         <li>
-          <button onClick={handleSignout}>Signout</button>
+          <button onClick={() => navigate("/")}>Signout</button>
         </li>
       </div>
     </NavStyled>
@@ -69,7 +59,6 @@ const NavStyled = styled.nav`
     display: flex;
     align-items: center;
     gap: 1rem;
-
     img {
       width: 80px;
       height: 80px;
@@ -80,7 +69,6 @@ const NavStyled = styled.nav`
       padding: 0.2rem;
       box-shadow: 0px 1px 17px rgba(0, 0, 0, 0.06);
     }
-
     h2 {
       color: rgba(34, 34, 96, 1);
     }
@@ -145,7 +133,7 @@ const NavStyled = styled.nav`
     padding: 0;
 
     li {
-      list-style-type: none;
+      list-style-type: none; 
     }
 
     button {
