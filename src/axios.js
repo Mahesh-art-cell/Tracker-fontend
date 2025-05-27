a0d6d83adb670c9e010fb8bf9fcab660c558de47
Mahesh Talluri <mtalluri563@gmail.com>
2025-05-27 10:40:51 +0530

    add

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://tracker-beckend.onrender.com/api/v1",
 });
 
 // Automatically attach token to every request
